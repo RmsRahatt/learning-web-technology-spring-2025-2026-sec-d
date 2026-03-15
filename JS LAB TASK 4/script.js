e86fb97b5d152446fac2acd7282284b1f312ecc7
MOSTAKIM, MD.RAHAT <23-55028-3@student.aiub.edu>
2026-03-15 14:47:31 +0600

    js

--- JS LAB TASK 4/script.js
+++ JS LAB TASK 4/script.js
@@ -1,1 +1,27 @@
+let quantity = document.getElementById("qty");
+let total = document.getElementById("total");
+
+let unitPrice = 1000;
+let coupon = 0;
+
+quantity.oninput = function () {
+  let qty = quantity.value;
+
+  if (qty < 0 || qty == "") {
+    quantity.value = 0;
+    qty = 0;
+  }
+
+  let totalPrice = unitPrice * qty;
+  total.value = totalPrice;
+
+  if (totalPrice > 1000 && coupon == 0) {
+    setTimeout(function () {
+      alert("Congratulations! You are eligible for a coupon.");
+    }, 100);
+    coupon = 1;
+  } else if (totalPrice <= 1000) {
+    coupon = 0;
+  }
+};
 
